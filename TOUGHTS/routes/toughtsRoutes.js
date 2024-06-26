a1e73358d9b41e8a0bc01aaa1dab0580addd765e
Ian Nakamura <16380276688@estudante.ufjf.br>
2024-06-26 13:59:49 -0300

    Criando formulário de pensamentos.

--- TOUGHTS/routes/toughtsRoutes.js
+++ TOUGHTS/routes/toughtsRoutes.js
@@ -5,6 +5,8 @@ const ToughtController = require('../controllers/ToughtController')
 
 const checkAuth = require('../helpers/auth').checkAuth
 
+router.get('/add', checkAuth, ToughtController.createTought)
+router.post('/add', checkAuth, ToughtController.createToughtSave)
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showToughts)
 
